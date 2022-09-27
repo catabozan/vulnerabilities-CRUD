9b677d320e0e7f5dd6c042165aee9aa20dbd09d4
Catalin Bozan <catabozan@protonmail.com>
2022-09-28 01:02:42 +0300

    fix: add threat level information to single vulnerability view

--- resources/views/vulnerabilities/show.blade.php
+++ resources/views/vulnerabilities/show.blade.php
@@ -6,7 +6,18 @@
     </x-slot>
 
     <div class="container bg-white rounded-lg mt-8 mx-auto p-8">
-        <span class="text-slate-400">Submitted by: {{ $vulnerability->user->name }}</span>
+        <div class="flex justify-between">
+            <span class="text-slate-400">Submitted by: {{ $vulnerability->user->name }}</span>
+            <div class="inline min-w-fit">
+                <x-badge :class="$vulnerability->threat_level === 0
+                    ? 'bg-lime-400'
+                    : ($vulnerability->threat_level === 1
+                        ? 'bg-amber-400'
+                        : 'bg-red-400')">
+                    Threat level: {{ $vulnerability->threat_level }}
+                </x-badge>
+            </div>
+        </div>
         <h2 class="font-semibold text-xl text-gray-800 mt-4">Description</h2>
         <p> {{ $vulnerability->description }} </p>
     </div>
