5dd1c0e712b8224be935550440b6bbda28571b23
Catalin Bozan <catabozan@protonmail.com>
2022-09-28 00:24:47 +0300

    refactor: create component for vulnerability list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,26 +15,6 @@
         </div>
     </div>
 
-    <div class="mt-4 mx-auto container flex flex-wrap justify-around">
-        @foreach ($vulnerabilities as $vulnerability)
-            <div class="p-8 m-2 bg-white rounded-lg max-w-sm shadow-sm">
-                <div class="flex justify-between">
-                    <h2 class="inline-block text-lg font-bold mr-2">{{ $vulnerability->title }}</h2>
-                    <div class="inline min-w-fit">
-                        <x-badge :class="$vulnerability->threat_level === 0
-                            ? 'bg-lime-400'
-                            : ($vulnerability->threat_level === 1
-                                ? 'bg-amber-400'
-                                : 'bg-red-400')">
-                            Threat: {{ $vulnerability->threat_level }}
-                        </x-badge>
-                    </div>
-                </div>
-                <p class="truncate mt-4">{{ $vulnerability->description }}</p>
-                <span class="block text-right text-slate-400 mt-1">
-                    ~ {{ $vulnerability->user->name }}
-                </span>
-            </div>
-        @endforeach
-    </div>
+    <x-vulnerability-list :vulnerabilities="$vulnerabilities" />
+
 </x-app-layout>
