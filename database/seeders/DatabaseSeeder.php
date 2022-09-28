c8797985d66e2251dce958f6ea24c5c7578d5798
Catalin Bozan <catabozan@protonmail.com>
2022-09-28 15:33:46 +0300

    feat: add seeder for top 10 owasp vulnerabilities

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Vulnerability;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,10 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('Pass1234'),
         ]);
+
+        $this->seedVulnerabilitiesFromJson($user);
+    }
+
+    protected function seedVulnerabilitiesFromJson(User $user)
+    {
+        $json = file_get_contents(__DIR__ . '/vulnerability_data.json');
+        $data = collect(json_decode($json, true));
+
+        $data->each(fn (array $vulnerabilityData) => Vulnerability::create([...$vulnerabilityData, 'user_id' => $user->getKey()]));
     }
 }
